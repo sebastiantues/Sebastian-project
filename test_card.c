1cc5ea9c9db444101a13baff778475460df4b868
User <user@ubuntu.ubuntu-domain>
2014-04-14 14:42:54 +0300

    homework1

--- test_card.c
+++ test_card.c
@@ -6,13 +6,13 @@ int main () {
 	int result;
 
 	strcpy(c1.name, "card 1");
-	c1.power = 0;
-	c1.life = 0;
+	c1.power = 7;
+	c1.life = 12;
 	c1.price = 43;
 
 	strcpy(c2.name, "card 2");
-	c2.power = 0;
-	c2.life = 0;
+	c2.power = 8;
+	c2.life = 2;
 	c2.price = 43;
 	
 	result = attack(&c1, &c2);
