3eba50382164e347acf48a3c0b18ec4286c30a55
Sebastian <sebastian2005@abv.bg>
2014-06-09 22:12:27 +0300

    Update manapool.h

--- manapool.h
+++ manapool.h
@@ -1,15 +1,20 @@
 #ifndef MANAPOOL_H
 #define MANAPOOL_H
+#define MAX_MANA 10
+#define INTIAL_MANA 1
 #include "card.h"
 #include "deck.h"
 
 struct manapool_t {
-	int price;
-	int max_price;
+	int left;
+	int max;
 };
 
+void init_manapool(struct manapool_t *manapool)
+void add_mana(struct manapool_t *manapool,int mana)
+void remove_mana(struct manapool_t *manapool,int mana);
+void add_max_mana(struct manapool_t *manapool,int mana);
+void put_mana(struct manapool_t *manapool);
 int can_put_card(struct card_t card_to_put, struct manapool_t pool);
-int can_use_card(struct card_t *card, struct manapool_t *pool);
-int change_mana(struct manapool_t *manapool,int del);
 
 #endif
