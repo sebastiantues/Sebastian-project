5a0239825f886a1700d8906a44c63f88ea1801d6
Sebastian <sebastian2005@abv.bg>
2014-06-09 22:05:05 +0300

    Update manapool.c

--- manapool.c
+++ manapool.c
@@ -1,14 +1,34 @@
 #include "manapool.h"
 
-int can_put_card(struct card_t card_to_put, struct manapool_t pool){
-	if(card_to_put.price <= pool.price) {
-		return 1;
-	} else {
-		return 0;
+
+void init_manapool(struct manapool_t *manapool) {
+	manapool->left = manapool->max=INTIAL_MANA;
+}
+void add_mana(struct manapool_t *manapool,int mana) {
+	manapool->max += mana;
+	if (manapool->max > MAX_MANA) {
+	       manapool->max += MAX_MANA;
 	}
 }
-int can_use_card(struct card_t *card, struct manapool_t *pool) {
+void remove_mana(struct manapool_t *manapool,int mana) {
+	manapool->max -= mana;
+	if (manapool->max < MAX_MANA) {
+	       manapool->max -= MAX_MANA;
+	}
 }
-int change_mana(struct manapool_t *manapool,int del) {
-	manapool-> price += del;
+void add_max_mana(struct manapool_t *manapool,int mana) {
+	manapool->max += mana;
+	if (manapool->max > MAX_MANA) {
+	       manapool->max = MAX_MANA;
+	}
+}
+void put_mana(struct manapool_t *manapool) {
+	manapool->left = manapool->max;
+}
+int can_put_card(struct card_t card_to_put, struct manapool_t pool){
+         int result = pool->left - card_to_put.mana_cost;
+         if (result>=0) {
+         	return 1;
+         }
+         return 0;
 }
